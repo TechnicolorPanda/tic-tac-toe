ae4dda73be1510ccdfed757fee38534f0d2decbc
Lorri Romesberg <LorriAnn7@att.net>
2020-05-13 21:26:03 -0400

    added playing board

--- tictactoe.js
+++ tictactoe.js
@@ -1,20 +1,41 @@
-
 const gameBoard = (function() {
     let spaces = [];
     const container = document.getElementById("container");
+    const content = document.createElement("div");
+    content.classList.add("box");
+    container.appendChild(content);
     for (i = 0; i<=2; i++) {
+        var row = document.createElement("div"); 
+        row.className = "row"; 
         for (j = 1; j<=3; j++) {
-            let cell = document.createElement("div");
-            cell.innerText = ((i*3)+j);
-            spaces.push((i*3)+j);
+            const cell = document.createElement("div");
+            cell.className = "gridsquare"; 
+            let spaceNumber = ((i*3)+j);
+            let choice = " ";
+            cell.addEventListener("click", selectSquare);
+            console.log("returned choice = " + choice);
+            cell.innerText = choice;
+            let playerChoice = {[spaceNumber]: [choice]};
+            let spaceArray = Object.assign({}, playerChoice);
+            spaces.push(spaceArray);
             container.appendChild(cell).className = "grid";
+            row.appendChild(cell); 
         }
+        content.appendChild(row);
     };
-    console.log (spaces);
+    console.log(spaces);
 }) ();
 
+function selectSquare(e) {
+    console.log("click");
+    //const thisCell = e.target;
+    let choice = "X";
+    console.log(choice);
+    return choice;
+}
+
 const displayController = (function() {
-    console.log("displayController")
+    
     }) ();
 
 const createUser = ({userName, avatar}) => ({

45461ccff6ec18545ee0b576d096033c9c764c70
Lorri Romesberg <LorriAnn7@att.net>
2020-05-23 20:40:03 -0400

    determines and displays winner of game

--- tictactoe.js
+++ tictactoe.js
@@ -6,6 +6,13 @@ const gameBoard = (function() {
     const content = document.createElement("div");
     content.classList.add("box");
     container.appendChild(content);
+    fillBoard(container, content);
+    const reset = document.getElementById("reset");
+    reset.addEventListener("click", resetGame);
+    return spaces;
+}) ();
+
+function fillBoard(container, content) {
     for (i = 0; i<=2; i++) {
         var row = document.createElement("div"); 
         row.className = "row"; 
@@ -26,8 +33,7 @@ const gameBoard = (function() {
         }
         content.appendChild(row);
     };
-    return spaces;
-}) ();
+}
 
 function selectSquare(e) {
     let thisCell = e.target;
@@ -49,30 +55,71 @@ function selectSquare(e) {
     let select = {"selection": choice};
     let spaceArray = Object.assign({}, select);
     spaces.splice(targetID, 1, spaceArray);
-    //checkWin();
-}
-
-function checkWin() {
-    console.log ("Check win = " + spaces[0].selection + spaces[1].selection + spaces[2].selection);
-    if (spaces[0].selection = spaces[1].selection = spaces[2].selection){
-        if (spaces[0].selection == "X") {
-            alert(playerX.userName + " Wins!")
-        } else {
-            alert(playerO.userName + " Wins!")  
-        }
-    }
-}
-
-const displayController = (function() {
     console.log(spaces);
-    if (spaces[0].selection === spaces[1].selection === spaces[2].selection){
-        if (spaces[0].selection === "X") {
-            alert(playerX.userName + " Wins!")
-        } else {
-            alert(playerO.userName + " Wins!")  
-        }
+    checkWin(spaces);
+}
+
+function checkWin(spaces) {
+    if (spaces[0].selection == "O" & spaces[1].selection == "O" & spaces[2].selection == "O") {
+        alert(playerO.userName + " Wins!");
+    } else if (spaces[3].selection == "O" & spaces[4].selection == "O" & spaces[5].selection == "O") {
+        alert(playerO.userName + " Wins!");
+    } else if (spaces[6].selection == "O" & spaces[7].selection == "O" & spaces[8].selection == "O") {
+        alert(playerO.userName + " Wins!");
+    } else if (spaces[0].selection == "O" & spaces[3].selection == "O" & spaces[6].selection == "O") {
+        alert(playerO.userName + " Wins!");
+    } else if (spaces[1].selection == "O" & spaces[4].selection == "O" & spaces[7].selection == "O") {
+        alert(playerO.userName + " Wins!");
+    } else if (spaces[2].selection == "O" & spaces[5].selection == "O" & spaces[8].selection == "O") {
+        alert(playerO.userName + " Wins!");
+    } else if (spaces[0].selection == "O" & spaces[4].selection == "O" & spaces[8].selection == "O") {
+        alert(playerO.userName + " Wins!");
+    } else if (spaces[6].selection == "O" & spaces[4].selection == "O" & spaces[2].selection == "O") {
+        alert(playerO.userName + " Wins!");
+    } else if (spaces[0].selection == "X" & spaces[1].selection == "X" & spaces[2].selection == "X") {
+        alert(playerX.userName + " Wins!");
+    } else if (spaces[3].selection == "X" & spaces[4].selection == "X" & spaces[5].selection == "X") {
+        alert(playerX.userName + " Wins!");
+    } else if (spaces[6].selection == "X" & spaces[7].selection == "X" & spaces[8].selection == "X") {
+        alert(playerX.userName + " Wins!");
+    } else if (spaces[0].selection == "X" & spaces[3].selection == "X" & spaces[6].selection == "X") {
+        alert(playerX.userName + " Wins!");
+    } else if (spaces[1].selection == "X" & spaces[4].selection == "X" & spaces[7].selection == "X") {
+        alert(playerX.userName + " Wins!");
+    } else if (spaces[2].selection == "X" & spaces[5].selection == "X" & spaces[8].selection == "X") {
+        alert(playerX.userName + " Wins!");
+    } else if (spaces[0].selection == "X" & spaces[4].selection == "X" & spaces[8].selection == "X") {
+        alert(playerX.userName + " Wins!");
+    } else if (spaces[6].selection == "X" & spaces[4].selection == "X" & spaces[2].selection == "X") {
+        alert(playerX.userName + " Wins!");
     }
-}) ();
+}
+
+function resetGame() {
+    console.log("reset");
+    for (let i=0; i<8; i++) {
+        cell.id = i;
+        let choice = "I";
+        let select = {"selection": choice};
+        cell.innerText = choice;
+        cell.style.color = "white";
+        let selectChoice = Object.assign({}, select);
+        spaces.push(selectChoice);
+    }
+    let turn = 0;
+
+}
+
+//const displayController = (function() {
+//    console.log(spaces);
+//    if (spaces[0].selection === spaces[1].selection === spaces[2].selection){
+//        if (spaces[0].selection === "X") {
+//            alert(playerX.userName + " Wins!")
+//        } else {
+//            alert(playerO.userName + " Wins!")  
+//        }
+//    }
+//}) ();
 
 const createUser = (userName, marker) => {
     return  {userName, marker};
@@ -81,5 +128,3 @@ const createUser = (userName, marker) => {
 const playerX = createUser("Player X", "X");
 
 const playerO = createUser("Player O", "O");
-
-
